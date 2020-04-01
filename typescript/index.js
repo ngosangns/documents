143e57576513ecf7fcf7c4af6b6a675a58ea70d0
NGO SANG <46277436+ngosangns@users.noreply.github.com>
2020-04-01 16:59:52 +0700

    Visitor

--- typescript/index.js
+++ typescript/index.js
@@ -1,34 +1,29 @@
 "use strict";
-class ConcreteObserver {
-    constructor(beforeMessage) {
-        this.beforeMessage = beforeMessage;
-    }
-    update(message) {
-        console.log(this.beforeMessage + " " + message);
+class AmericanLady {
+    accept(visitor) {
+        visitor.visit(this);
     }
 }
-class ConcreteSubject {
-    constructor() {
-        this.observers = new Array();
-    }
-    attach(observer) {
-        this.observers.push(observer);
-    }
-    detach(observer) {
-        this.observers.splice(this.observers.indexOf(observer), 1);
-    }
-    notifyChange(message) {
-        for (let observer of this.observers) {
-            observer.update(message);
-        }
+class JapanLady {
+    accept(visitor) {
+        visitor.visit(this);
     }
 }
-let subject = new ConcreteSubject();
-let observer1 = new ConcreteObserver("Message 1 updated:");
-let observer2 = new ConcreteObserver("Message 2 updated:");
-subject.attach(observer1);
-subject.attach(observer2);
-subject.notifyChange('Subject notify!');
-subject.detach(observer1);
-console.log("Removed Observer 1\n");
-subject.notifyChange('Subject notify!');
+class SayLoveVisitor {
+    visit(lady) {
+        if (lady instanceof AmericanLady)
+            console.log('I love you');
+        if (lady instanceof JapanLady)
+            console.log('Aishite imasu');
+    }
+}
+class SayGoodByeVisitor {
+    visit(lady) {
+        if (lady instanceof AmericanLady)
+            console.log('Good bye!');
+        if (lady instanceof JapanLady)
+            console.log('Sayounara!');
+    }
+}
+let lady = new JapanLady();
+lady.accept(new SayGoodByeVisitor());
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,41 +1,40 @@
-interface Observer {
-    update(mesage: string): void
+interface Lady {
+    accept(visitor: Visitor): void
 }
 
-class ConcreteObserver implements Observer {
-    constructor(private beforeMessage: string) {}
-    update(message: string) {
-        console.log(this.beforeMessage + " " + message)
-    }
-}
-interface Subject {
-    observers : Array<Observer>
-    attach(observer: Observer) : void
-    detach(observer: Observer): void
-    notifyChange(message: string): void
-}
-class ConcreteSubject implements Subject {
-    observers: Array<Observer> = new Array<Observer>()
-    attach(observer: Observer): void {
-        this.observers.push(observer)
-    }
-    detach(observer: Observer): void {
-        this.observers.splice(this.observers.indexOf(observer), 1)
-    }
-    notifyChange(message: string): void {
-        for (let observer of this.observers) {
-            observer.update(message)
-        }
+class AmericanLady implements Lady {
+    accept(visitor: Visitor): void {
+        visitor.visit(this)
     }
 }
 
-let subject : Subject = new ConcreteSubject()
-let observer1 : Observer = new ConcreteObserver("Message 1 updated:")
-let observer2 : Observer = new ConcreteObserver("Message 2 updated:")
-subject.attach(observer1)
-subject.attach(observer2)
+class JapanLady implements Lady {
+    accept(visitor: Visitor): void {
+        visitor.visit(this)
+    }
+}
 
-subject.notifyChange('Subject notify!')
-subject.detach(observer1)
-console.log("Removed Observer 1\n")
-subject.notifyChange('Subject notify!')
+interface Visitor {
+    visit(lady: Lady): void
+}
+
+class SayLoveVisitor implements Visitor {
+    visit(lady: Lady): void {
+        if (lady instanceof AmericanLady)
+            console.log('I love you')
+        if (lady instanceof JapanLady)
+            console.log('Aishite imasu')
+    }
+}
+
+class SayGoodByeVisitor implements Visitor {
+    visit(lady: Lady): void {
+        if (lady instanceof AmericanLady)
+            console.log('Good bye!')
+        if (lady instanceof JapanLady)
+            console.log('Sayounara!')
+    }
+}
+
+let lady: Lady = new JapanLady()
+lady.accept(new SayGoodByeVisitor())
